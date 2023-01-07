0ea8b8b0300304cd6d4fadd4fafdf8e89cbd297f
Thu Nguyen <19522308@gm.uit.edu.vn>
2023-01-07 20:00:13 +0700

    ..

--- HabitTracking/HabitTracking/Popups/IconCategoryPopup.xaml.cs
+++ HabitTracking/HabitTracking/Popups/IconCategoryPopup.xaml.cs
@@ -16,6 +16,10 @@ namespace HabitTracking.Popups
     public partial class IconCategoryPopup : Popup
     {
         Category categorySelected = new Category();
+        public IconCategoryPopup()
+        {
+            InitializeComponent();
+        }
         public IconCategoryPopup(Category category)
         {
             InitializeComponent();
